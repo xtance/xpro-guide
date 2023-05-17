f09819f1648bf3389afcd976cd0cbb1b62c95d0e
xtance <ni77ty@gmail.com>
2023-05-18 01:23:52 +0300

    фиксы!

--- src/lib/Subtitle.svelte
+++ src/lib/Subtitle.svelte
@@ -1,3 +1,3 @@
-<div class="text-lg lg:text-2xl 2xl:text-4xl opacity-50 text-center leading-loose mt-4">
+<div class="text-lg lg:text-2xl 2xl:text-4xl opacity-50 text-center mt-4" style="line-height: 1.5;">
 	<slot/>
 </div>
